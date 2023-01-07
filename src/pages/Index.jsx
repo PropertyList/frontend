68cd1e4030ae3bdbf65a33ee7f89e5c39ce6648a
Pratham <prathamsinghal2003@gmail.com>
2023-01-07 14:23:48 +0530

    :sparkles: Added personal Details page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import PersonalDetails from "./PersonalDetails";
 const Index = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/kyc" element={<PersonalDetails />} />
       </Routes>
     </BrowserRouter>
   );
