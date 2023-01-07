946511ddd4bc5a2ed6ac7cce57e4c6b905613f34
Pratham <prathamsinghal2003@gmail.com>
2023-01-08 02:53:49 +0530

    :art: Revamped buyer

--- src/pages/Buyer.jsx
+++ src/pages/Buyer.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import Header from "../components/SellerHeader";
-import ProfilePic from "../assets/profileimg.png";
+import ProfilePic from "../assets/user2.png";
 import Listings from "../components/MyListings";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Buyer = () => {
   const navigate = useNavigate();
-  const {user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   return (
     <div>
       <Header img={ProfilePic} />
       <div className="flex flex-row">
         <div className="basis-[23%] border-r-2 border-[#E5E5E5] flex flex-col h-[90vh]">
-        {isAuthenticated && <img
-            src={user.picture}
-            alt="profile-pic"
-            className="w-48 h-48 mx-auto my-6 rounded-[50%]"
-          />}
-          {isAuthenticated && <h2 className="mx-auto text-[#0D1D54] font-semibold text-xl my-2">{user.name}</h2>}
+          {isAuthenticated && (
+            <img
+              src={user.picture}
+              alt="profile-pic"
+              className="w-48 h-48 mx-auto my-6 rounded-[50%]"
+            />
+          )}
+          {isAuthenticated && (
+            <h2 className="mx-auto text-[#0D1D54] font-semibold text-xl my-2">
+              {user.name}
+            </h2>
+          )}
           {/* <h2 className="mx-auto text-[#0D1D54] font-semibold text-xl my-2">
             Tammy Hills
           </h2> */}
-          <h2 className="mx-auto text-[#404B7C] text-xl ">Buyer</h2>
-          <h2 className="mx-auto text-[#0D1D54] font-semibold text-xl my-2">
+          <h2 className="mx-auto text-[#404B7C] text-2xl mt-5 ">Buyer</h2>
+          <h2 className="mx-auto text-[#0D1D54] font-semibold text-2xl my-2">
             Total Profit
           </h2>
-          <h2 className="mx-auto text-[#green] text-xl ">$100,000</h2>
+          <h2 className="mx-auto text-[#green] text-2xl ">$100,000</h2>
         </div>
         <div className="basis-[77%]">
           <div className="flex flex-row justify-between my-10 px-10">
@@ -39,7 +45,7 @@ const Buyer = () => {
                 navigate("/list");
               }}
             >
-              Start Investing +
+              Start Investing
             </button>
           </div>
           <Listings />
