3053264c0cce029ff7a0946966198f504fe45a4a
Pratham <prathamsinghal2003@gmail.com>
2023-01-08 03:33:24 +0530

    :art: Revamped properties page

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const PropertyCard = (props) => {
   return (
-    <div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src={props.img} alt="card-img" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{props.name}</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-          <button>Bid Now</button>
-        </div>
+    <div className="max-w-[280px] h-[400px] my-2 rounded-xl overflow-hidden shadow-lg mr-5">
+      <img className="w-full h-[45%]" src={props.img} alt="card-img" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{props.name}</div>
+        <p className="text-gray-700 text-base">{props.ownership} Ownership</p>
+        <p className="text-gray-700 text-base">Price: {props.price}</p>
+        <p className="text-gray-700 text-base">
+          Est. income per month: {props.est}
+        </p>
+      </div>
+      <div className="px-6  pb-2">
+        <button className="my-2 p-3 bg-[#6B8418] text-white rounded-xl w-full">
+          Bid Now
+        </button>
       </div>
     </div>
   );
--- src/pages/AllProperties.jsx
+++ src/pages/AllProperties.jsx
@@ -2,22 +2,61 @@ import React from "react";
 import Header from "../components/PropertyHeader";
 import User from "../assets/user2.png";
 import Cards from "../components/PropertyCard";
-import H1 from "../assets/house/1.png";
+import H1 from "../assets/house/new1.jpg";
+import H2 from "../assets/house/new2.jpeg";
+import H3 from "../assets/house/new3.jpg";
+
 const AllProperties = () => {
   return (
     <div>
       <Header img={User} />
       <div className="flex flex-col p-7">
-        <h2 className="text-xl font-semibold text-[#0D1D54] mt-4 mb-2 ">
+        <h2 className="text-2xl font-semibold text-[#0D1D54] mt-4 mb-2 ">
           Properties near you
         </h2>
-        <Cards img={H1} name="NYC property" />
-        <h2 className="text-xl font-semibold text-[#0D1D54] my-2">
+        <Cards
+          img={H1}
+          name="NYC property"
+          price="$22,000"
+          est="$999"
+          ownership="2%"
+        />
+        <h2 className="text-2xl font-semibold text-[#0D1D54] my-2">
           Buy Home fractions
         </h2>
-        <h2 className="text-xl font-semibold text-[#0D1D54] my-2">
+        <div className="flex flex-row ">
+          <Cards
+            img={H2}
+            name="NYC property"
+            price="$22,000"
+            est="$999"
+            ownership="2%"
+          />
+          <Cards
+            img={H3}
+            name="NYC property"
+            price="$22,000"
+            est="$999"
+            ownership="2%"
+          />
+          <Cards
+            img={H1}
+            name="NYC property"
+            price="$22,000"
+            est="$999"
+            ownership="2%"
+          />
+        </div>
+        <h2 className="text-2xl font-semibold text-[#0D1D54] my-2">
           Invest Togther
         </h2>
+        <Cards
+          img={H3}
+          name="NYC property"
+          price="$22,000"
+          est="$999"
+          ownership="2%"
+        />
       </div>
     </div>
   );
